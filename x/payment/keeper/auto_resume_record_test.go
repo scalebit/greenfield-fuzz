cd00797a6bcd61f14f12495e32605003abdb44b6
guojinliang <guojinliangchn@gmail.com>
2023-10-24 13:36:54 +0800

    FuzzAutoResumeRecord

--- x/payment/keeper/auto_resume_record_test.go
+++ x/payment/keeper/auto_resume_record_test.go
@@ -1,6 +1,7 @@
 package keeper_test
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -49,4 +50,47 @@ func TestAutoResumeRecord(t *testing.T) {
 	require.True(t, !exist)
 	exist = keeper.ExistsAutoResumeRecord(ctx, 0, addr2)
 	require.True(t, !exist)
+}
+
+func FuzzAutoResumeRecord(f *testing.F) {
+	f.Add(int64(100))
+	f.Fuzz(func(t *testing.T, a int64) {
+		keeper, ctx, _ := makePaymentKeeper(t)
+		if a < 2 {
+			return
+		}
+
+		addr1 := sample.RandAccAddress()
+		record1 := &types.AutoResumeRecord{
+			Addr:      addr1.String(),
+			Timestamp: a,
+		}
+
+		// set
+		keeper.SetAutoResumeRecord(ctx, record1)
+
+		// exits
+		// before the timestamp
+		rand.Seed(a)
+		n1 := 1 + rand.Int63n(a-1)
+		exist := keeper.ExistsAutoResumeRecord(ctx, n1, addr1)
+		// t.Log(n1)
+		require.True(t, !exist)
+		n2 := a + rand.Int63()
+		exist = keeper.ExistsAutoResumeRecord(ctx, n2, addr1)
+		// t.Log(n2)
+		require.True(t, exist)
+
+		// at any time
+		exist = keeper.ExistsAutoResumeRecord(ctx, 0, addr1)
+		require.True(t, exist)
+
+		// remove
+		keeper.RemoveAutoResumeRecord(ctx, record1.Timestamp, addr1)
+
+		exist = keeper.ExistsAutoResumeRecord(ctx, 0, addr1)
+		require.True(t, !exist)
+
+	})
+
 }
