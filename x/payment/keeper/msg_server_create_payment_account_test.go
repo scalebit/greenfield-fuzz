9add3450fa0108d744d07b0ce4c8f70fbba5d5a2
guojinliang <guojinliangchn@gmail.com>
2023-10-24 16:25:29 +0800

    FuzzCreatePaymentAccount

--- x/payment/keeper/msg_server_create_payment_account_test.go
+++ x/payment/keeper/msg_server_create_payment_account_test.go
@@ -1,6 +1,8 @@
 package keeper_test
 
 import (
+	"testing"
+
 	"github.com/bnb-chain/greenfield/testutil/sample"
 	"github.com/bnb-chain/greenfield/x/payment/types"
 )
@@ -32,4 +34,48 @@ func (s *TestSuite) TestCreatePaymentAccount() {
 	msg = types.NewMsgCreatePaymentAccount(creator.String())
 	_, err = s.msgServer.CreatePaymentAccount(s.ctx, msg)
 	s.Require().Error(err)
+}
+
+func FuzzCreatePaymentAccount(f *testing.F) {
+	f.Add(uint64(2))
+	f.Fuzz(func(t *testing.T, a uint64) {
+		s := &TestSuite{}
+		s.SetT(t)
+		s.SetupTest()
+
+		creator := sample.RandAccAddress()
+
+		// create first one
+		msg := types.NewMsgCreatePaymentAccount(creator.String())
+		_, err := s.msgServer.CreatePaymentAccount(s.ctx, msg)
+		s.Require().NoError(err)
+
+		record, _ := s.paymentKeeper.GetPaymentAccountCount(s.ctx, creator)
+		s.Require().True(record.Count == uint64(1))
+
+		// limit the number of payment account
+		params := s.paymentKeeper.GetParams(s.ctx)
+		params.PaymentAccountCountLimit = a
+		_ = s.paymentKeeper.SetParams(s.ctx, params)
+
+		for i := uint64(2); i <= a; i++ {
+			// create another one
+			msg = types.NewMsgCreatePaymentAccount(creator.String())
+			_, err = s.msgServer.CreatePaymentAccount(s.ctx, msg)
+
+			s.Require().NoError(err)
+
+			record, _ = s.paymentKeeper.GetPaymentAccountCount(s.ctx, creator)
+			s.Require().True(record.Count == i)
+		}
+
+		msg = types.NewMsgCreatePaymentAccount(creator.String())
+		_, err = s.msgServer.CreatePaymentAccount(s.ctx, msg)
+		t.Log(a, err)
+		if a == 0 {
+			return
+		}
+		s.Require().Error(err)
+	})
+
 }
